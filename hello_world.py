79da50314da76ec13b1950deec9f336e79fa5b45
Flesss <144239064+Flesss@users.noreply.github.com>
2023-09-14 15:22:55 +0900

    Four files are commited

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
+def print_hello():
+    print("Hello World!!")
 
-# In[ ]:
-
-
-print("Hello, World!!!!!!!")
-
+if __name__ == '__main__':
+    print_hello()
+    
